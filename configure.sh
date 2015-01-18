209c9a30af5e5f7f0ac53465fd71a387955e22fa
Adam Long <macadam.long@gmail.com>
2015-01-18 11:31:59 +0000

    Automatically builds allwpilib if it isn't detected

--- configure.sh
+++ configure.sh
@@ -11,8 +11,20 @@ if [ ! -d bin/ ] ;then
 fi
 
 if [ ! -d /var/frc/wpilib/ ] ;then
-    echo ">> WPILib not found in /var/frc/. Creating a clone"
+    echo ">> WPILib headers not found"
     echo ">> **root is required for writing to /var/frc initally (will be chmodded)"
     sudo mkdir -p /var/frc && sudo chmod -R 707 /var/frc
     git clone https://github.com/stonewareslord/wpilib.git /var/frc/wpilib
+fi
+
+if [ ! -a /usr/local/include/WPILib.h ] ;then
+    echo ">>Complete WPILib library not found. Building..."
+    git clone https://usfirst.collab.net/gerrit/allwpilib /tmp/allwpilib
+    cd /tmp/allwpilib && mkdir build && cd build
+    cmake .. -DCMAKE_TOOLCHAIN_FILE=../arm-toolchain.cmake
+    make
+    echo "*****************************************"
+    echo ">>Root is required to install the library"
+    echo "*****************************************"
+    cd /tmp/allwpilib/build && sudo make install
 fi
