62aa32c4b4d651d52875680cb6fc30d5970c5aed
Adam Long <macadam.long@gmail.com>
2015-01-19 18:35:25 +0000

    compiles the compiler automagically

--- configure.sh
+++ configure.sh
@@ -17,14 +17,84 @@ if [ ! -d /var/frc/wpilib/ ] ;then
     git clone https://github.com/stonewareslord/wpilib.git /var/frc/wpilib
 fi
 
-if [ ! -a /usr/local/include/WPILib.h ] ;then
-    echo ">>Complete WPILib library not found. Building..."
-    git clone https://usfirst.collab.net/gerrit/allwpilib /tmp/allwpilib
-    cd /tmp/allwpilib && mkdir build && cd build
-    cmake .. -DCMAKE_TOOLCHAIN_FILE=../arm-toolchain.cmake
-    make
-    echo "*****************************************"
-    echo ">>Root is required to install the library"
-    echo "*****************************************"
-    cd /tmp/allwpilib/build && sudo make install
+#if [ ! -a /usr/local/include/WPILib.h ] ;then
+#    if [ $(which cmake) ] ;then
+#        echo ">>Complete WPILib library not found. Building..."
+#        git clone https://usfirst.collab.net/gerrit/allwpilib /tmp/allwpilib
+#        cd /tmp/allwpilib && mkdir build && cd build
+#        cmake .. -DCMAKE_TOOLCHAIN_FILE=../arm-toolchain.cmake
+#        make
+#        echo "*****************************************"
+#        echo ">>Root is required to install the library"
+#        echo "*****************************************"
+#        cd /tmp/allwpilib/build && sudo make install
+#        sudo rm -r /tmp/allwpilib/
+#    fi
+#fi
+
+if [ ! -a /usr/bin/arm-frc-linux-gnueabi-g++ ] ;then
+    if [ $(which hg) ] && [ $(which cmake) ] && [ $(which wget) ];then
+        echo ">>cmake and hg found"
+        echo ">>installing frcmake-hg"
+        mkdir ~/frctoolchain && cd ~/frctoolchain
+        hg clone https://bitbucket.org/byteit101/toolchain-builder
+        cd toolchain-builder/tools && sudo make -f frcmake-nix-makefile install
+        cd ~/frctoolchain/ && wget ftp://gcc.gnu.org/pub/gcc/releases/gcc-4.9.1/gcc-4.9.1.tar.bz2
+        tar -xvjf gcc-4.9.1.tar.bz2
+        cd gcc-4.9.1 && mkdir gcc-build  && cd gcc-build
+        ../configure \
+            --prefix=/usr \
+            --program-prefix=arm-frc-linux-gnueabi- \
+            --target=arm-frc-linux-gnueabi \
+            --host=$CHOST \
+            --build=$CHOST \
+            --enable-shared \
+            --disable-nls \
+            --enable-threads=posix \
+            --enable-languages=c,c++ \
+            --disable-multilib \
+            --disable-multiarch \
+            --with-sysroot=/usr/arm-frc-linux-gnueabi \
+            --with-build-sysroot=/usr/arm-frc-linux-gnueabi \
+            --with-as=/usr/bin/arm-frc-linux-gnueabi-as \
+            --with-ld=/usr/bin/arm-frc-linux-gnueabi-ld \
+            --with-arch=armv7-a \
+            --with-cpu=cortex-a9 \
+            --with-float=softfp \
+            --with-fpu=vfp \
+            --with-specs='%{save-temps: -fverbose-asm} %{funwind-tables|fno-unwind-tables|mabi=*|ffreestanding|nostdlib:;:-funwind-tables}' \
+            --enable-lto \
+            --with-pkgversion='GCC for FRC' \
+            --with-cloog \
+            --disable-multiarch \
+            --enable-poison-system-directories \
+           --enable-plugin \
+            --with-system-zlib \
+            --disable-libmudflap
+        clear && sleep 1 
+        echo ">>Initalizing build of gcc"
+        sleep 1 && clear
+        make all-gcc all-target-libstdc++-v3
+        sudo make install-gcc install-target-libgcc install-target-libstdc++-v3
+        clear && sleep 1
+        echo ">>Installing arm-frc-linux-gnuabi-binutils"
+        sleep 1 && clear
+        cd ~/frctoolchain && wget ftp://ftp.gnu.org/gnu/binutils/binutils-2.24.tar.bz2 && tar -xvjf binutils-2.24.tar.bz2 && cd binutils-2.24
+        find -name config.cache -exec rm {} \;
+        ./configure \
+            --prefix=/usr \
+            --target=arm-frc-linux-gnueabi \
+            --with-pkgversion='GNU Binutils for FRC' \
+            --with-sysroot=/usr/arm-frc-linux-gnueabi \
+            --disable-multilib \
+            --disable-nls \
+            --enable-lto \
+            --disable-libiberty-install \
+            --disable-werror
+        make configure-host
+        make
+        sudo make install
+    else
+        echo ">>Please install cmake and hg"
+    fi
 fi
